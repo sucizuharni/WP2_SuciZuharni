039befac28bfeeffd47723711a2b748111c6c285
sucizuharni <suci280602z@gmail.com>
2023-04-06 08:49:15 +0700

    menambahkan Latihan1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Suci Zuharni
+    Saya tinggal di daerah Depok
+    olahraga yang saya sukai adalah
+    Lari";
+ }
+}
