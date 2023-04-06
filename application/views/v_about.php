a4ced1708a23c53b42fbf4da662946cce9823c97
sucizuharni <suci280602z@gmail.com>
2023-04-06 11:06:57 +0700

    menambahkan Latihan1

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Suci</li>
+ <li>Nama Belakang : Zuharni</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan jambu, beji, pondok cina</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Tanjung Pinang</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Lari</li>
+ <li>Bulu Tangkis</li>
+ </ul>
+</section>
